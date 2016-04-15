b0b52d93a0dd14b4313716f39d14b26f5efc7e17
Joey Murphy <murphyjm@stanford.edu>
2016-04-15 09:49:15 -0700

    Finished all of lab6

--- tower.py
+++ tower.py
@@ -7,9 +7,13 @@ from scipy import integrate
 
 t, v = np.loadtxt('droptower_vdata.txt', unpack=True)
 x = integrate.cumtrapz(v, t, initial=0)
-a = list(np.diff(v))
+diffs = list(np.diff(v))
 
-a = [0] + a
+dt = t[1] - t[0]
+a = []
+a.append(diffs[0]/dt)
+for dv in diffs:
+	a.append(dv/dt)
 
 z = (x, v, a)
 labels = ('position', 'velocity', 'acceleration')
@@ -17,6 +21,14 @@ labels = ('position', 'velocity', 'acceleration')
 for y, name in zip(z, labels):
 	plt.plot(t,y, label=name)
 
+x_axis = np.zeros(len(x))
+plt.plot(t, x_axis, '--')
 plt.legend()
 plt.title('Droptower Kinematics')
+
+a = [elem for elem in a if elem > 0] # Filter out acceleration values < 0
+sum_a = np.sum(a)
+avg_a = sum_a/len(a)
+print('Average upward acceleration = %f\n') %avg_a
+
 plt.show()
